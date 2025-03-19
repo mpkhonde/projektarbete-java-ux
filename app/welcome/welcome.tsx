2bf7ff1558105d4ddef417addbe977d373cf8343
davidlkarlsson <david.karlsson@stud.sti.se>
2025-03-19 13:35:04 +0100

    Added: testing different css-styles for no spend week application

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,14 +1,14 @@
-import {MultiColorButtons} from "~/components/MultiColorButtons";
-
+import { MultiColorButtons } from "~/components/MultiColorButtons";
 
 export function Welcome() {
-
-
   return (
-      <>
+    <main>
+      <div className="nospendweek"></div>
+      <h2>No spend-week</h2>
+      <p>Har du spenderat något denna vecka? Klicka på knappen för vald dag (GRÖN = SPENDERAT, RÖD = INTE SPENDERAT) </p>
+      <div className="buttoncontainer">
         <MultiColorButtons />
-      </>
+      </div>
+    </main>
   );
 }
-
-    
