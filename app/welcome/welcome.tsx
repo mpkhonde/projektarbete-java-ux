da2b6436fae97d8edce537d3700f07dd6429d4d2
davidlkarlsson <david.karlsson@stud.sti.se>
2025-03-19 14:02:19 +0100

    Fix: css styling no spend week

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,9 +3,10 @@ import { MultiColorButtons } from "~/components/MultiColorButtons";
 export function Welcome() {
   return (
     <main>
-      <div className="nospendweek"></div>
+      <div className="nospendweek">
       <h2>No spend-week</h2>
-      <p>Har du spenderat något denna vecka? Klicka på knappen för vald dag (GRÖN = SPENDERAT, RÖD = INTE SPENDERAT) </p>
+      <p>Har du spenderat något denna vecka? (GRÖN = INTE SPENDERAT, RÖD = SPENDERAT) </p>
+      </div>
       <div className="buttoncontainer">
         <MultiColorButtons />
       </div>
