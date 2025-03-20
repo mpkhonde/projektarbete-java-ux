0fd23d680a787d24b6e873161feb4585a9464314
LinusHolmer <linus.holmer@stud.sti.se>
2025-03-20 09:50:28 +0100

    Update welcome.tsx

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,7 +5,7 @@ export function Welcome() {
     <main>
       <div className="no-spend-week">
       <h2>No spend-week</h2>
-      <p>Har du spenderat något denna vecka? GRÖN = INTE SPENDERAT<br/>RÖD = SPENDERAT </p>
+      <p>Har du spenderat något denna vecka? <br/> GRÖN = INTE SPENDERAT<br/>RÖD = SPENDERAT </p>
       </div>
       <div className="button-container">
         <MultiColorButtons />
