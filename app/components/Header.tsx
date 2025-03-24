58135bfb8953a4129eeadf3d3e53cf450f75eabe
davidlkarlsson <david.karlsson@stud.sti.se>
2025-03-24 10:25:48 +0100

    Update: added transition to header-element when scrolling - David & Linnea

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,21 @@
 import styles from "./Header.module.css";
 import logo from "../images/balanza_logo_2.png"
+import { useEffect, useState } from "react";
 export function Header() {
+
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+          setHasScrolled(window.scrollY > 10);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${hasScrolled ? styles.scrolled : ""}`}>
 
         <img src={logo} alt="Bild på en logotyp för Balanza" height="80px" width="auto"/>
       
