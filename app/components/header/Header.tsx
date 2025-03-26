41bb50bde4d65e258dd39f26a020c626e3fb8953
davidlkarlsson <david.karlsson@stud.sti.se>
2025-03-26 13:48:58 +0100

    Fix: error causing vercel to not show website

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,28 +1,31 @@
 import styles from "./Header.module.css";
-import logo from "../../images/balanza_logo_2.png"
+import logo from "../../images/balanza_logo_2.png";
 import { useEffect, useState } from "react";
-import NavBar from "../navbar/navBar";
+import NavBar from "../navbar/NavBar";
 export function Header() {
-
   const [hasScrolled, setHasScrolled] = useState(false);
 
   useEffect(() => {
-      const handleScroll = () => {
-          setHasScrolled(window.scrollY > 10);
-      };
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 10);
+    };
 
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-  
+
   return (
-    <header className={`${styles.header} ${hasScrolled ? styles.scrolled : ""}`}>
+    <header
+      className={`${styles.header} ${hasScrolled ? styles.scrolled : ""}`}
+    >
+      <img
+        src={logo}
+        alt="Bild på en logotyp för Balanza"
+        height="80px"
+        width="auto"
+      />
 
-        <img src={logo} alt="Bild på en logotyp för Balanza" height="80px" width="auto"/>
-
-        <NavBar/>
-
-      
+      <NavBar />
     </header>
   );
 }
